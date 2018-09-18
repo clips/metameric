0116aad92acff359018e21829d254374b2db6a44
stephantul <stephan.tulkens@uantwerpen.be>
2018-09-18 16:42:26 +0200

    push metric, setup

--- setup.py
+++ setup.py
@@ -1,9 +1,24 @@
 # -*- coding: utf-8 -*-
 """Setup file."""
+import sys
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Distutils import build_ext
 import numpy as np
+
+
+cython = False
+
+if "--cython" in sys.argv:
+    cython = True
+    sys.argv.remove("--cython")
+
+if cython:
+    from Cython.Build import cythonize
+    extensions = cythonize([Extension("tilapia.core.metric",
+                            ["tilapia/core/metric.pyx"])])
+else:
+    extensions = [Extension("tilapia.core.metric",
+                            ["tilapia/core/metric.c"])]
 
 setup(name='tilapia',
       version='1.0.2',
@@ -12,16 +27,10 @@ setup(name='tilapia',
       author_email='stephan.tulkens@uantwerpen.be',
       url='https://github.com/stephantul/tilapia',
       license='MIT',
-      install_requires=['numpy>=1.11.0', 'cython'],
       classifiers=[
           'Intended Audience :: Developers',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 3'],
       keywords='computational psycholinguistics neural networks',
-      zip_safe=False,
-      cmdclass={'build_ext': build_ext},
-      ext_modules=[Extension("tilapia.core.metric",
-                             ["tilapia/core/metric.pyx"])],
-      include_dirs=[np.get_include()],
-      include_package_data=True
-      )
+      ext_modules=extensions,
+      include_dirs=[np.get_include()])
