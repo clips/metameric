4c3cb87879f3cff9e5baeee64f8402ad22dea0bf
stephantul <stephan.tulkens@uantwerpen.be>
2018-09-19 11:31:20 +0200

    fix runner, reading of files

--- tilapia/run.py
+++ tilapia/run.py
@@ -1,25 +1,27 @@
 """Make an IA run."""
 import numpy as np
 
-from tilapia.ia.utils import IA_WEIGHTS, prep_words
-from tilapia.ia.utils import weight_adaptation, weights_to_matrix
+from tilapia.prepare.weights import weight_adaptation, weights_to_matrix
+from tilapia.prepare.weights import IA_WEIGHTS
 from tilapia.builder import build_model
 from itertools import chain
 from collections import Counter
-from csv import reader
 
 
 def read_input_file(f):
     """Read an input file."""
     items = []
-    if f.mode.startswith('rb'):
-        f = reader((x.decode('utf-8') for x in f))
-    else:
-        f = reader(f)
     header = next(f)
-    for line in f:
+    decode = False
+    if isinstance(header, bytes):
+        header = header.decode('utf-8').strip().split(",")
+        decode = True
+    else:
+        header = header.strip().split(",")
+
+    for line in (l.decode('utf-8') if decode else l for l in f):
         item = {}
-        for k, v in zip(header, line):
+        for k, v in zip(header, line.strip().split(",")):
             item[k] = v.split()
         items.append(item)
 
@@ -27,7 +29,6 @@ def read_input_file(f):
         if all([len(i[k]) == 1 for i in items]):
             for i in items:
                 i[k] = i[k][0]
-
     return items
 
 
@@ -47,7 +48,6 @@ def get_model(words_file,
               parameters,
               rla_variable,
               rla_layers,
-              input_layers,
               output_layers,
               global_rla,
               step_size,
@@ -62,9 +62,6 @@ def get_model(words_file,
 
     weights = weight_adaptation(max_length, weights)
     matrix, names = weights_to_matrix(weights)
-
-    if 'features' in names and 'features' not in words[0]:
-        words = prep_words(words)
 
     for w in words:
         for idx in range(len(w['orthography']), max_length):
@@ -81,7 +78,6 @@ def get_model(words_file,
                     global_rla,
                     outputs=output_layers,
                     step_size=step_size,
-                    inputs=input_layers,
                     decay_rate=decay_rate,
                     minimum=minimum_activation)
 
@@ -95,7 +91,6 @@ def make_run(words_file,
              threshold,
              rla_variable,
              rla_layers,
-             input_layers,
              output_layers,
              global_rla,
              step_size,
@@ -104,12 +99,10 @@ def make_run(words_file,
              minimum_activation):
     """Method for running."""
     test_words = read_input_file(test_words_file)
-
     m = get_model(words_file,
                   parameters,
                   rla_variable,
                   rla_layers,
-                  input_layers,
                   output_layers,
                   global_rla,
                   step_size,
@@ -118,14 +111,6 @@ def make_run(words_file,
 
     keys_words = Counter(chain.from_iterable(test_words))
     keys_in_all = [k for k, v in keys_words.items() if v == len(test_words)]
-
-    if 'features' in m.inputs and 'features' not in test_words[0]:
-        test_words = prep_words(test_words)
-
-    for word in test_words:
-        if set(input_layers) - set(word):
-            raise ValueError("{} does not contain all input layers."
-                             "".format(word))
 
     results = m.activate_bunch(test_words,
                                max_cycles=max_cycles,
