fcd8643e89b9b950c6f49fb3afe9f1760119c4c1
stephantul <stephan.tulkens@uantwerpen.be>
2018-09-20 15:55:19 +0200

    change visualization

--- tilapia/plot/plot.py
+++ tilapia/plot/plot.py
@@ -2,15 +2,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from itertools import chain
 
-
-def plot_result(result, max_cycles=None):
+def plot_result(result, node_names, max_cycles=None, minimum=-.2):
     """Plot the activations of a single word, and show the plot."""
     result_plot(result, max_cycles=max_cycles).show()
 
 
-def result_plot(result, max_cycles=None):
+def result_plot(result, node_names, max_cycles=None, minimum=-.2):
     """Plot the activations of a single word."""
     keys = list(result.keys())
     if max_cycles is None:
@@ -25,18 +23,17 @@ def result_plot(result, max_cycles=None):
     for idx, (key, plot) in enumerate(zip(keys, plots)):
 
         data = result[key]
-        all_words = set(chain.from_iterable(data))
-        data_dict = {w: [0] for w in all_words}
+        names = node_names[key]
 
-        for x in range(max_cycles):
-            for w in all_words:
-                data_dict[w].append(data[x].get(w, -1))
+        idxes = np.max(data, 0) > .0
+        data = data[:, idxes]
+        names = [names[idx] for idx in np.flatnonzero(idxes)]
 
-        for k, v in data_dict.items():
+        for k, v in zip(names, data.T):
             plot.plot(v)
             plot.annotate(k, (max_cycles * np.random.uniform(.5, .9), v[-1]))
         plot.set_title(key)
-        plot.set_ylim(.0, 1.0)
+        plot.set_ylim(minimum, 1.0)
         if idx == 0:
             plot.set_ylabel("Activation")
         plot.set_xlabel("Cycles")
