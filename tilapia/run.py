b8ce18c818956cdd605815fe54d9d420fcc9c600
stephantul <stephan.tulkens@uantwerpen.be>
2018-09-27 17:21:45 +0200

    add pandas as dependency, make reading files better

--- tilapia/run.py
+++ tilapia/run.py
@@ -1,36 +1,69 @@
 """Make an IA run."""
 import numpy as np
+import pandas as pd
 
-from tilapia.prepare.weights import weight_adaptation, weights_to_matrix
+from tilapia.prepare.weights import weights_to_matrix
 from tilapia.prepare.weights import IA_WEIGHTS
-from tilapia.builder import build_model
+from tilapia.builder import Builder
 from itertools import chain
 from collections import Counter
 
 
+def make_slot(x):
+    """Turn a list of slot features into slots."""
+    for value in x:
+        value = value.split("-")
+        yield((value[0], int(value[1])))
+
+
+def is_slot(x):
+    """Check whether a feature is slot-based."""
+    for value in x:
+        s = value.split("-")
+        if len(s) != 2:
+            return False
+        try:
+            int(s[1])
+        except ValueError:
+            return False
+    return True
+
+
 def read_input_file(f):
     """Read an input file."""
-    items = []
-    header = next(f)
-    decode = False
-    if isinstance(header, bytes):
-        header = header.decode('utf-8').strip().split(",")
-        decode = True
-    else:
-        header = header.strip().split(",")
-
-    for line in (l.decode('utf-8') if decode else l for l in f):
-        item = {}
-        for k, v in zip(header, line.strip().split(",")):
-            item[k] = v.split()
-        items.append(item)
-
-    for k in header:
-        i = [i[k].split("-") for i in items]
-        if all([len(x) == 2 for x in i]):
+    df = pd.read_csv(f)
+    dtypes = [col for col, dtype in zip(df.columns, df.dtypes)
+              if dtype == object]
+    items = df.to_dict('records')
+    for d in dtypes:
+        slot_feature = []
+        for i in items:
+            i[d] = i[d].split()
+            slot_feature.append(is_slot(i[d]))
+        if all(slot_feature):
             for i in items:
-                i[k] = [x.split("-") for x in i[k]]
+                i[d] = list(make_slot(i[d]))
+
     return items
+
+
+def write_output_file(path, items, columns):
+    """Write the output."""
+    for item in items:
+        for k, v in item.items():
+            try:
+                try:
+                    if isinstance(item[k][0], tuple):
+                        item[k] = ["-".join([x[0], str(x[1])])
+                                   for x in item[k]]
+                except IndexError:
+                    pass
+                item[k] = " ".join(item[k])
+            except TypeError:
+                pass
+
+    d = pd.DataFrame(items)
+    d[columns].to_csv(path, index=False)
 
 
 def parse_parameter_file(f):
@@ -45,7 +78,7 @@ def parse_parameter_file(f):
     return weights
 
 
-def get_model(words_file,
+def get_model(items_file,
               parameters,
               rla_variable,
               rla_layers,
@@ -60,37 +93,28 @@ def get_model(words_file,
         print("Defaulting to standard IA parameters.")
         weights = IA_WEIGHTS
 
-    words = read_input_file(words_file)
-    max_length = max([len(w['orthography']) for w in words])
-
-    if adapt_weights:
-        weights = weight_adaptation(max_length, weights)
+    items = read_input_file(items_file)
     matrix, names = weights_to_matrix(weights)
-
-    for w in words:
-        for idx in range(len(w['orthography']), max_length):
-            w['letters'].append(" -{}".format(idx))
-        w['orthography'] = w['orthography'].ljust(max_length)
 
     rla = {k: 'global' if k not in rla_layers
            else rla_variable for k in names}
 
-    m = build_model(words,
-                    names,
-                    matrix,
-                    rla,
-                    global_rla,
-                    outputs=output_layers,
-                    monitors=monitor_layers,
-                    step_size=step_size,
-                    decay_rate=decay_rate,
-                    minimum=minimum_activation)
+    m = Builder(names,
+                matrix,
+                rla,
+                global_rla,
+                outputs=output_layers,
+                monitors=monitor_layers,
+                step_size=step_size,
+                decay_rate=decay_rate,
+                minimum=minimum_activation,
+                weight_adaptation=adapt_weights).build_model(items)
 
     return m
 
 
-def make_run(words_file,
-             test_words_file,
+def make_run(items_file,
+             test_items_file,
              output_path,
              parameters,
              threshold,
@@ -105,8 +129,8 @@ def make_run(words_file,
              minimum_activation,
              adapt_weights):
     """Method for running."""
-    test_words = read_input_file(test_words_file)
-    m = get_model(words_file,
+    test_items = read_input_file(test_items_file)
+    m = get_model(items_file,
                   parameters,
                   rla_variable,
                   rla_layers,
@@ -118,10 +142,11 @@ def make_run(words_file,
                   minimum_activation,
                   adapt_weights)
 
-    keys_words = Counter(chain.from_iterable(test_words))
-    keys_in_all = [k for k, v in keys_words.items() if v == len(test_words)]
+    keys_items = Counter(chain.from_iterable(test_items))
+    columns = [k for k, v in keys_items.items() if v == len(test_items)]
+    columns.append("cycles")
 
-    results = m.activate_bunch(test_words,
+    results = m.activate_bunch(test_items,
                                max_cycles=max_cycles,
                                threshold=threshold,
                                strict=False)
@@ -131,10 +156,7 @@ def make_run(words_file,
     right = cycles < max_cycles
     cycles[~right] = -1
 
-    with open(output_path, 'w') as f:
-        k = ",".join([o for o in keys_in_all])
-        f.write("{},cycles\n".format(k))
-        for a, b in zip(test_words, cycles):
-            a = ",".join([x if isinstance(x, str) else " ".join(x)
-                          for x in [a[k] for k in keys_in_all]])
-            f.write("{},{}\n".format(a, b))
+    for i, c in zip(test_items, cycles):
+        i["cycles"] = c
+
+    write_output_file(output_path, test_items, columns)
