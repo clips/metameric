56de1b2ed74815f73d8386db0d849122c477d668
stephantul <stephan.tulkens@uantwerpen.be>
2018-09-27 17:22:34 +0200

    NEW builder

--- tilapia/builder/builder.py
+++ tilapia/builder/builder.py
@@ -3,51 +3,7 @@ import numpy as np
 
 from ..core import Network
 from itertools import chain, product
-from collections import Counter
-
-
-class Item(object):
-    """An item class."""
-    def __init__(self):
-        self.data = {}
-        self.meta = {}
-
-    def __getitem__(self, key):
-        return self.data[key]
-
-    def add_meta(self, key, value):
-        self.meta[key] = value
-
-    def add_data(self, key, value):
-        self.data[key] = value
-
-    def get_correspondence(self, source, dest):
-        """Get the correspondence between items."""
-        if source not in self.data or dest not in self.data:
-            raise ValueError("{} or {} not present".format(source, dest))
-        s = self.data[source]
-        d = self.data[dest]
-        if self.is_sequence(source) and self.is_sequence(dest):
-            return [(x[0], y[0]) for x, y in
-                    filter(lambda x: x[0][1] == x[1][1], product(s, d))]
-        else:
-            return product(s, d)
-
-    def is_decomposed(self, key):
-        """Check whether the layer is decomposed."""
-        return not self.is_feature(key) and self.is_sequence(key)
-
-    def is_sequence(self, key):
-        """Check whether a key is a sequence."""
-        return all([len(x) == 2 and isinstance(x[1], int)
-                    for x in self.data[key]])
-
-    def is_feature(self, key):
-        """Check whether a key is a feature."""
-        if not self.is_sequence(key):
-            return False
-        a, b = zip(*self.data[key])
-        return any([x > 1 for x in Counter(b).values()])
+from collections import Counter, defaultdict
 
 
 class Builder(object):
@@ -80,7 +36,6 @@ class Builder(object):
     """
 
     def __init__(self,
-                 items,
                  layer_names,
                  weight_matrix,
                  rla,
@@ -89,7 +44,8 @@ class Builder(object):
                  monitors=(),
                  minimum=-.2,
                  step_size=1.0,
-                 decay_rate=.07):
+                 decay_rate=.07,
+                 weight_adaptation=True):
         """Build a model out of a set of items."""
         if weight_matrix.shape[0] != weight_matrix.shape[1]:
             raise ValueError("Weight matrix must be square, is {}"
@@ -101,13 +57,6 @@ class Builder(object):
                              "".format(weight_matrix.shape[0],
                                        len(layer_names)))
 
-        # Gather all unique items.
-        self._check(items, layer_names)
-        out_layers = set(outputs) - set(layer_names)
-        if out_layers:
-            raise ValueError("Not all outputs were in your layer names."
-                             "".format(out_layers))
-
         self.layer_names = layer_names
         self.weight_matrix = weight_matrix
         self.rla = rla
@@ -117,82 +66,48 @@ class Builder(object):
         self.minimum = minimum
         self.step_size = step_size
         self.decay_rate = decay_rate
+        self.weight_adaptation = weight_adaptation
 
-        self.items = []
-        for i in items:
-            it = Item()
-            for k, v in i.items():
-                if k in self.layer_names:
-                    it.add_data(k, v)
-                else:
-                    it.add_meta(k, v)
-            self.items.append(it)
+    def is_sequence(self, item):
+        """Check whether a key is a sequence."""
+        for x in item:
+            if len(x) != 2 or not isinstance(x[1], int):
+                return False
+        return True
 
-        layers = [x.data.keys() for x in self.items]
-        self.num_slots = {}
-        self.layers = set(chain.from_iterable(layers))
-        self.sequence = {k for k in self.layers
-                         if all([i.is_sequence(k) for i in self.items])}
-        self.feature = {k for k in self.layers
-                        if any([i.is_feature(k) for i in self.items])}
-        self.decomposed = {k for k in self.layers
-                           if any([i.is_decomposed(k) for i in self.items])}
-        self.unique_items = {k: list(set(self.item_sequence(k)))
-                             for k in self.layers}
+    def is_feature(self, item):
+        """Check whether a key is a feature."""
+        if not self.is_sequence(item):
+            return False
+        a, b = zip(*item)
+        return any([x > 1 for x in Counter(b).values()])
 
-    def item_sequence(self, key):
+    def item_sequence(self, items, key):
         """Get items as a sequence."""
-        d = chain.from_iterable([i[key] for i in self.items])
-        if key not in self.sequence:
+        d = chain.from_iterable([i[key] for i in items])
+        if key not in self.slot_layers:
             return sorted(d)
         else:
             d, idx = zip(*d)
-            self.num_slots[key] = max(idx) + 1
-            res = []
-            for x in range(self.num_slots[key]):
-                res.extend([(d_, x) for d_ in d])
-            return res
-
-    def get_correspondence(self, key, key_b):
-        """Get correspondence between a and b."""
-        k_1 = {k: idx for idx, k in enumerate(self.unique_items[key])}
-        k_2 = {k: idx for idx, k in enumerate(self.unique_items[key_b])}
-
-        mtr = np.zeros((len(k_1), len(k_2))) - 1
-
-        for i in self.items:
-            for a, b in i.get_correspondence(key, key_b):
-                if key in self.sequence and key_b in self.sequence:
-                    k = key if key in self.decomposed else key_b
-                    for l in range(self.num_slots[k]):
-                        mtr[k_1[(a, l)], k_2[(b, l)]] = 1
-                else:
-                    mtr[k_1[a], k_2[b]] = 1
-
-        if key in self.sequence and key_b in self.sequence:
-            for (a, slot_1) in self.unique_items[key]:
-                for (b, slot_2) in self.unique_items[key_b]:
-                    if slot_1 != slot_2:
-                        mtr[k_1[(a, slot_1)], k_2[(b, slot_2)]] = 0
-
-        return mtr
+            self.num_slots[key] = max(max(idx)+1, self.num_slots[key])
+            return sorted(d)
 
     def get_node_names(self, key):
         """Get the node names of a layer."""
         return self.unique_items[key]
 
-    def sum_over(self, key, field_to_sum):
+    def sum_over(self, items, key, field_to_sum):
         """Sum over a field for a given key."""
         k_1 = {k: idx for idx, k in enumerate(self.unique_items[key])}
         sums = np.zeros(len(k_1))
-        for i in self.items:
-            f = i.meta[field_to_sum]
+        for i in items:
+            f = i[field_to_sum]
             for x in i[key]:
                 sums[k_1[x]] += f
 
         return sums
 
-    def build_model(self):
+    def build_model(self, items):
         """
 
         Returns
@@ -201,13 +116,47 @@ class Builder(object):
             An initialized network.
 
         """
+        # Gather all unique items.
+        self._check(items, self.layer_names)
+        out_layers = set(self.outputs) - set(self.layer_names)
+        if out_layers:
+            raise ValueError("Not all outputs were in your layer names."
+                             "".format(out_layers))
+
         # Initialize the TilapIA.
         m = Network(minimum=self.minimum,
                     step_size=self.step_size,
                     decay_rate=self.decay_rate)
 
-        # Iterate over all unique items.
+        self.num_slots = defaultdict(int)
+
+        self.feature_layers = set()
+        self.slot_layers = set()
         for k in self.layer_names:
+            for i in items:
+                if self.is_feature(i[k]):
+                    self.feature_layers.add(k)
+                    break
+            for i in items:
+                if self.is_sequence(i[k]):
+                    self.slot_layers.add(k)
+                    break
+
+        self.unique_items = {k: set(self.item_sequence(items, k))
+                             for k in self.layer_names}
+
+        for k in self.slot_layers:
+            u = self.unique_items[k]
+            if k not in self.feature_layers:
+                u.add(" ")
+                pass
+            self.unique_items[k] = sorted(u)
+
+        self.unique_items = {k: {x: idx for idx, x in enumerate(v)}
+                             for k, v in self.unique_items.items()}
+
+        # Iterate over all unique items.
+        for k in sorted(self.layer_names):
 
             # Determine resting level activation.
             # If the resting is denoted as "global", every
@@ -215,7 +164,7 @@ class Builder(object):
             if self.rla[k] == 'global':
                 resting = np.ones(len(self.unique_items[k])) * self.global_rla
             else:
-                resting = self.sum_over(k, self.rla[k])
+                resting = self.sum_over(items, k, self.rla[k])
                 if resting.min() < 1:
                     resting += (1 - resting.min())
                 resting = np.log10(resting)
@@ -223,6 +172,12 @@ class Builder(object):
                 resting = self.global_rla * (1.0 - resting)
 
             node_names = self.unique_items[k]
+            if k in self.slot_layers:
+                resting = np.concatenate([resting] * self.num_slots[k])
+                n = []
+                for idx in range(self.num_slots[k]):
+                    n.extend([(x, idx) for x in node_names])
+                node_names = n
 
             m.create_layer(k,
                            resting,
@@ -238,9 +193,72 @@ class Builder(object):
             if not pos and not neg:
                 continue
 
-            mtr = self.get_correspondence(a, b)
-            mtr[mtr == 1] = pos
-            mtr[mtr == -1] = neg
+            a_slot = a in self.slot_layers
+            b_slot = b in self.slot_layers
+
+            d = a_slot or b_slot
+            f = a in self.feature_layers or b in self.feature_layers
+
+            if d and not f:
+                true_num_slots = max(self.num_slots.get(a, 0),
+                                     self.num_slots.get(b, 0))
+                pos = pos / true_num_slots
+                neg = neg * true_num_slots
+
+            u_a = self.unique_items[a]
+            u_b = self.unique_items[b]
+            num_u_a = len(u_a)
+            num_u_b = len(u_b)
+
+            if a_slot and not b_slot:
+                dim_a = num_u_a * self.num_slots.get(a, 1)
+            else:
+                dim_a = num_u_a
+            if not a_slot and b_slot:
+                dim_b = num_u_b * self.num_slots.get(b, 1)
+            else:
+                dim_b = num_u_b
+            mtr = np.zeros((dim_a,
+                            dim_b))
+            mtr = mtr + neg
+            for i in items:
+                a_values = i[a]
+                b_values = i[b]
+                if a_slot and b_slot:
+                    for ((x, i1), (y, i2)) in product(a_values, b_values):
+                        if i1 != i2:
+                            continue
+                        mtr[(u_a[x], u_b[y])] = pos
+
+                    if a not in self.feature_layers:
+                        for x, y in b_values:
+                            if x.endswith("neg"):
+                                mtr[u_a[" "], u_b[x]] = pos
+
+                    if b not in self.feature_layers:
+                        for x, y in a_values:
+                            if x.endswith("neg"):
+                                mtr[u_a[x], u_b[" "]] = pos
+                else:
+                    if a_slot:
+                        idx_a = [u_a[x] + (num_u_a * y) for x, y in a_values]
+                    else:
+                        idx_a = [u_a[x] for x in a_values]
+                    if b_slot:
+                        idx_b = [u_b[x] + (num_u_b * y) for x, y in b_values]
+                    else:
+                        idx_b = [u_b[x] for x in b_values]
+                    mtr[np.ix_(idx_a, idx_b)] = pos
+
+            if a_slot and b_slot:
+                x, y = mtr.shape
+                new_mtr = np.zeros((x * self.num_slots[a],
+                                    y * self.num_slots[b]))
+                for idx in range(self.num_slots[a]):
+                    s_a, e_a = x * idx, x * (idx + 1)
+                    s_b, e_b = y * idx, y * (idx + 1)
+                    new_mtr[s_a:e_a, s_b:e_b] = mtr
+                mtr = new_mtr
 
             m.connect_layers(a, b, mtr)
 
