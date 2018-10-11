f8f12ef5199b50078cc0664fde5ccf21f612103c
stephantul <stephan.tulkens@uantwerpen.be>
2018-10-11 15:21:44 +0200

    remove weights_to_matrix, change weight format to dict

--- diploria/prepare/__init__.py
+++ diploria/prepare/__init__.py
@@ -1,9 +1,8 @@
 """Prepare word lists for analysis using diploria."""
 from .data import process_data, process_and_write
-from .weights import weights_to_matrix, IA_WEIGHTS
+from .weights import IA_WEIGHTS
 
 
 __all__ = ["process_data",
            "process_and_write",
-           "weights_to_matrix",
            "IA_WEIGHTS"]
--- diploria/prepare/weights.py
+++ diploria/prepare/weights.py
@@ -1,23 +1,7 @@
 """Utilities specifically for IA models."""
-import numpy as np
-from itertools import chain
-
 
 # Dict format
 IA_WEIGHTS = {("letters", "orthography"): [.28, -.01],
               ("orthography", "letters"): [1.2, .0],
               ("orthography", "orthography"): [.0, -.21],
               ("letters-features", "letters"): [.005, -.15]}
-
-
-def weights_to_matrix(weights):
-    """Convert a dictionary to a matrix."""
-    all_keys = set(chain.from_iterable(weights.keys()))
-    mtr = np.zeros((len(all_keys), len(all_keys), 2))
-    main_key = {k: idx for idx, k in enumerate(all_keys)}
-    for (a, b), value in weights.items():
-        mtr[main_key[a], main_key[b]] = value
-
-    k, _ = zip(*sorted(main_key.items(), key=lambda x: x[1]))
-
-    return mtr, k
