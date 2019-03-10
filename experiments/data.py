ebfb26bcea06369c2c5ad39d28543d5a9d0790f9
stephantul <stephan.tulkens@uantwerpen.be>
2019-03-10 20:36:52 +0100

    Fix data in experiments

--- experiments/data.py
+++ experiments/data.py
@@ -8,12 +8,10 @@ def read_elp_format(filename, lengths=()):
     """Read RT data from the ELP."""
     temp = set()
     lengths = set(lengths)
-    df = pd.read_csv(filename)
+    df = pd.read_csv(filename, keep_default_na=True)
     df = df.dropna(subset=["Word", "I_Mean_RT", "SUBTLWF"])
     for idx, line in df.iterrows():
         if line['Word'] in temp:
-            continue
-        if np.isnan(line['I_Mean_RT']):
             continue
         if set(line['Word']) - set(ascii_lowercase):
             continue
